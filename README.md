fcdeaede3e1bde81c887847a27a078354905495d
Daniel Duvanå <36152732+Daniel-BD@users.noreply.github.com>
2024-03-21 11:48:44 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# screen_reader_examples
+ 
---
+++ android/app/src/main/kotlin/com/example/screen_reader_examples/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.screen_reader_examples
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
